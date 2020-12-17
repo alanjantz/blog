dbdf0dd7c3780a30b81b6418efa623d6a9f8d3dc
Alan Jantz <alan.jantz@ambevtech.com.br>
2020-12-17 17:39:14 -0300

    Separação entre página inicial e blog

---
+++ src/components/MainHeader/MainHeader.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./styles";
+
+const MainHeader: React.FC = () => {
+  return (
+    <>
+      <Container>AAAAA</Container>
+    </>
+  );
+};
+
+export default MainHeader;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,15 @@
 import React, { Component } from "react";
-import { graphql } from "gatsby";
-import Layout from "../components/Layout";
-import Posts from '../components/Posts';
-import SEO from '../components/SEO';
-import 'minireset.css';
+import SEO from "../components/SEO";
+import "minireset.css";
+import MainHeader from '../components/MainHeader';
 
 export default class Index extends Component {
   render() {
-    const { data } = this.props;
     return (
-      <Layout>
+      <>
         <SEO />
-        <Posts data={data}/>
-      </Layout>
-    )
+        <MainHeader />
+      </>
+    );
   }
-};
-
-export const pageQuery = graphql`
-  query {
-    allMarkdownRemark(
-      limit: 2000
-      sort: { fields: [fields___prefix], order: DESC }
-      filter: { frontmatter: { draft: { ne: true } } }
-      ) {
-      edges {
-        node {
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            tags
-            date(formatString: "DD/MM/YYYY")
-            description
-          }
-        }
-      }
-    }
-  }
-`;
+}
