56d79d4df93b8f93001637f6fb556d8b5b11cef2
Alan Jantz <alan.jantz@ambevtech.com.br>
2020-12-18 15:25:06 -0300

    Ajuste CNAME

--- data/config.js
+++ data/config.js
@@ -3,11 +3,10 @@ const config = {
   siteDescription: 'Personal blog',
   siteTitleShort: 'alanjantz.dev',
   siteRss: "/rss.xml",
-  siteImage: "https://codestackbrasil.netlify.com/banner.png",
   googleAnalyticsID: "UA-136490553-1",
   themeColor: "#00E2BC",
   backgroundColor: "#FFFFFF",
-  siteUrl: "https://codestackbrasil.netlify.com",
+  siteUrl: "https://alanjantz.github.io",
 };
 
 module.exports = config;
