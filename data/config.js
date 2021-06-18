dd4be5bf8cab66fe1368e11151117a6fd6eb2819
Alan Jantz <alan_f_jantz@hotmail.com>
2021-06-18 14:29:44 -0300

    Ajustes

--- data/config.js
+++ data/config.js
@@ -1,12 +1,12 @@
 const config = {
-  siteTitle: 'alanjantz.dev',
-  siteDescription: 'Personal blog',
-  siteTitleShort: 'alanjantz.dev',
+  siteTitle: "alanjantz.dev",
+  siteDescription: "Personal blog",
+  siteTitleShort: "alanjantz.dev",
   siteRss: "/rss.xml",
   googleAnalyticsID: "UA-136490553-1",
   themeColor: "#00E2BC",
   backgroundColor: "#FFFFFF",
-  siteUrl: "https://alanjantz.github.io",
+  siteUrl: "https://alanjantz.github.io/blog",
 };
 
 module.exports = config;
