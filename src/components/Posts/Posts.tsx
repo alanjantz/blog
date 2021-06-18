91663f12cec4567e892b64f01b08b59adef61ae2
Alan Jantz <alan_f_jantz@hotmail.com>
2021-06-18 14:53:50 -0300

    Ajuste diretório de postagens

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -11,7 +11,7 @@ const Posts: React.FC<PostsModel> = ({ data }) => {
   const posts = (
     edges.map(({ node }) => (
       <div key={node.fields.slug} className="post">
-        <Link to={node.fields.slug} className="post-title"><h2>{node.frontmatter.title}</h2></Link>
+        <Link to={'blog' + node.fields.slug} className="post-title"><h2>{node.frontmatter.title}</h2></Link>
         <p className="post-date">{node.frontmatter.date}</p>
         <p>{node.frontmatter.description}</p>
       </div>
